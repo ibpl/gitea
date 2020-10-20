ibpl/gitea

commit 27dc5a19e1f7e76f0b7a29ad72b633bea7f6b864

Inital language setting on reverse proxy login
Gitea does not update user language on first login when
in reverse proxy mode; this causes errors when user tries
to update their settings without setting language.

This mod sets language in reverse mode also like for
regular logins in routers/user/auth.go > handleSignInFull().

Fixes: bd8361daad082728a000a931f2a0f7e0dedadfa8
Author-Change-Id: IB#1104925

modules/auth/sso/reverseproxy.go
@@ -6,6 +6,7 @@
 package sso
 
 import (
+	"fmt"
 	"strings"
 
 	"code.gitea.io/gitea/models"
@@ -86,6 +87,14 @@ func (r *ReverseProxy) VerifyAuthData(ctx *macaron.Context, sess session.Store) 
 				log.Error("UserSignIn: %v", err)
 			}
 			return nil
+		}
+	}
+
+	// If the user does not have a locale set, we save the current one.
+	if len(user.Language) == 0 {
+		user.Language = ctx.Locale.Language()
+		if err = models.UpdateUserCols(user, "language"); err != nil {
+			log.Error(fmt.Sprintf("VerifyAuthData: error updating user language [user: %d, locale: %s]", user.ID, user.Language))
 		}
 	}
 

commit 45ea55dd71a5ee85264ac68a2246a7d2b6cf9c54

Fixed user session initialization on login with reverse proxy header
Gitea does not initialize user session after login using
reverse proxy header. This fixes it.

Fixes: 27dc5a19e1f7e76f0b7a29ad72b633bea7f6b864
Author-Change-Id: IB#1104925

modules/auth/sso/reverseproxy.go
@@ -98,6 +98,17 @@ func (r *ReverseProxy) VerifyAuthData(ctx *macaron.Context, sess session.Store) 
 		}
 	}
 
+	ctx.SetCookie("lang", user.Language, nil, setting.AppSubURL, setting.SessionConfig.Domain, setting.SessionConfig.Secure, true)
+
+	// Clear whatever CSRF has right now, force to generate a new one.
+	ctx.SetCookie(setting.CSRFCookieName, "", -1, setting.AppSubURL, setting.SessionConfig.Domain, setting.SessionConfig.Secure, true)
+
+	// Register last login.
+	user.SetLastLogin()
+	if err = models.UpdateUserCols(user, false, "last_login_unix"); err != nil {
+		log.Error(fmt.Sprintf("VerifyAuthData: error updating user last login time [user: %d]", user.ID))
+	}
+
 	return user
 }
 