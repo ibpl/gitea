ibpl/gitea

commit 99021feefc01b7c59d1b7a25d85a2c296b585ea0

API calls authorized with HTTP header
This mod allows API calls to be authorized with HTTP header
when ENABLE_REVERSE_PROXY_AUTHENTICATION is enabled. Without
it user authenticated by reverse proxy is able to access
gitea UI but not API which is inconsistent.

Author-Change-Id: IB#1107572

routers/api/v1/api.go
@@ -188,6 +188,10 @@ func reqToken() macaron.Handler {
 			return
 		}
 		if ctx.IsSigned {
+			// Don't require token if already authenticated by reverse proxy.
+			if setting.Service.EnableReverseProxyAuth {
+				return
+			}
 			ctx.RequireCSRF()
 			return
 		}

commit 636fa81840e11c6c9b109492e5ff355c2a10dcfb

Fixed API calls authorized with HTTP header
Only reqBasicAuth is modified to allow reverse proxy
auth as alternative and reqToken is left untouched.

Fixes: 99021feefc01b7c59d1b7a25d85a2c296b585ea0
Author-Change-Id: IB#1107572

routers/api/v1/api.go
@@ -21,7 +21,7 @@
 //     - text/html
 //
 //     Security:
-//     - BasicAuth :
+//     - BasicOrReverseProxyAuth :
 //     - Token :
 //     - AccessToken :
 //     - AuthorizationHeaderToken :
@@ -30,8 +30,9 @@
 //     - TOTPHeader :
 //
 //     SecurityDefinitions:
-//     BasicAuth:
-//          type: basic
+//     BasicOrReverseProxyAuth:
+//          type: basicOrReverseProxy
+//          description: Basic auth or rexerse proxy auth using HTTP header.
 //     Token:
 //          type: apiKey
 //          name: token
@@ -59,7 +60,7 @@
 //          type: apiKey
 //          name: X-GITEA-OTP
 //          in: header
-//          description: Must be used in combination with BasicAuth if two-factor authentication is enabled.
+//          description: Must be used in combination with BasicOrReverseProxyAuth if two-factor authentication is enabled.
 //
 // swagger:meta
 package v1
@@ -199,10 +200,13 @@ func reqToken() macaron.Handler {
 	}
 }
 
-func reqBasicAuth() macaron.Handler {
+func reqBasicOrRevProxyAuth() macaron.Handler {
 	return func(ctx *context.APIContext) {
+		if ctx.IsSigned && setting.Service.EnableReverseProxyAuth {
+			return
+		}
 		if !ctx.Context.IsBasicAuth {
-			ctx.Error(http.StatusUnauthorized, "reqBasicAuth", "basic auth required")
+			ctx.Error(http.StatusUnauthorized, "reqBasicOrRevProxyAuth", "auth required")
 			return
 		}
 		ctx.CheckForOTP()
@@ -553,7 +557,7 @@ func RegisterRoutes(m *macaron.Macaron) {
 					m.Combo("").Get(user.ListAccessTokens).
 						Post(bind(api.CreateAccessTokenOption{}), user.CreateAccessToken)
 					m.Combo("/:id").Delete(user.DeleteAccessToken)
-				}, reqBasicAuth())
+				}, reqBasicOrRevProxyAuth())
 			})
 		})
 